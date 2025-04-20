fixed numerical error in unicycle integration

socialjym/envs/base_env.py
@@ -24,6 +24,7 @@ HUMAN_POLICIES = [
 ROBOT_KINEMATICS = [
     "holonomic",
     "unicycle"]
+EPSILON = 1e-5 # Small value to avoid math overflow
 
 @jit
 def wrap_angle(theta:float) -> float:
@@ -302,7 +303,7 @@ class BaseEnv(ABC):
                 state[-1,1]+action[1]*self.humans_dt]))
         elif self.kinematics == ROBOT_KINEMATICS.index("unicycle"):
             new_state = lax.cond(
-                action[1] != 0,
+                jnp.abs(action[1]) > EPSILON,
                 lambda x: x.at[-1].set(jnp.array([
                     state[-1,0]+(action[0]/action[1])*(jnp.sin(state[-1,4]+action[1]*self.humans_dt)-jnp.sin(state[-1,4])),
                     state[-1,1]+(action[0]/action[1])*(jnp.cos(state[-1,4])-jnp.cos(state[-1,4]+action[1]*self.humans_dt)),

socialjym/policies/cadrl.py
@@ -6,7 +6,7 @@ from types import FunctionType
 import optax
 
 from .base_policy import BasePolicy
-from socialjym.envs.base_env import ROBOT_KINEMATICS, wrap_angle
+from socialjym.envs.base_env import EPSILON, ROBOT_KINEMATICS, wrap_angle
 
 VN_PARAMS = {
     "output_sizes": [150, 100, 100, 1],
@@ -157,7 +157,7 @@ class CADRL(BasePolicy):
             obs = obs.at[0:2].set(obs[0:2] + obs[2:4] * self.dt)
         elif self.kinematics == ROBOT_KINEMATICS.index('unicycle'):
             obs = lax.cond(
-                obs[3] != 0,
+                jnp.abs(obs[3]) > EPSILON,
                 lambda x: x.at[:].set(jnp.array([
                     x[0] + (x[2]/x[3]) * (jnp.sin(x[5] + x[3] * self.dt) - jnp.sin(x[5])),
                     x[1] + (x[2]/x[3]) * (jnp.cos(x[5]) - jnp.cos(x[5] + x[3] * self.dt)),

tests/test_actor_critic_policy.py
@@ -16,7 +16,8 @@ from socialjym.utils.aux_functions import animate_trajectory, load_crowdnav_poli
 random_seed = 0 
 n_episodes = 50
 kinematics = "unicycle"
-distribution = "gaussian"
+distribution = "dirichlet"
+v_max = 2
 tests_n_humans = [5, 10, 15, 20, 25]
 n_trials = 100
 ds = 0.2 # Discomfort distance
@@ -25,6 +26,7 @@ reward_function = Reward2(
         target_reached_reward = True,
         collision_penalty_reward = True,
         discomfort_penalty_reward = True,
+        v_max = v_max,
         progress_to_goal_reward = 1,
         time_penalty_reward = 0,
         high_rotation_penalty_reward = 0,
@@ -33,7 +35,7 @@ reward_function = Reward2(
     )
 env_params = {
     'robot_radius': 0.3,
-    'n_humans': 15,
+    'n_humans': 25,
     'robot_dt': 0.25,
     'humans_dt': 0.01,
     'robot_visible': True,
@@ -49,7 +51,7 @@ env_params = {
 env = SocialNav(**env_params)
 
 ### Initialize robot policy
-policy = SARLPPO(env.reward_function, dt=env_params['robot_dt'], kinematics=kinematics, distribution=distribution)
+policy = SARLPPO(env.reward_function, v_max=v_max, dt=env_params['robot_dt'], kinematics=kinematics, distribution=distribution)
 with open(os.path.join(os.path.dirname(__file__), 'rl_out.pkl'), 'rb') as f:
     rl_out = pickle.load(f)
     actor_params = rl_out['actor_params']
@@ -87,7 +89,9 @@ for i in range(n_episodes):
     while outcome["nothing"]:
         action, policy_key, _, _, distr = policy.act(policy_key, obs, info, actor_params, False)
         print(distr)
+        # print("Action: ", action)
         state, obs, info, reward, outcome, _ = env.step(state,info,action,test=True) 
+        # print("Robot state: ", state[-1,:])
         # print(f"Return in steps [0,{info['step']}):", info["return"])
         all_states = np.vstack((all_states, [state]))
 

tests/test_dirichlet.py
@@ -7,9 +7,9 @@ from socialjym.utils.distributions.dirichlet import Dirichlet
 
 random_seed = 0
 n_samples = 1_000
-vmax = 1
+vmax = 2
 wheels_distance = 0.7
-alpha = jnp.array([1., 1., 1.])
+alpha = jnp.array([1.0100837,   1.       , 951.1318])
 concentration = jnp.sum(alpha)
 epsilon = 1e-3
 

tests/test_sarl_ppo_training.py
@@ -24,11 +24,12 @@ n_trials = 1000
 n_parallel_envs = 50 
 training_updates = 10_000
 rl_debugging_interval = 10
+robot_vmax = 2
 training_hyperparams = {
     'random_seed': 0,
     'kinematics': 'unicycle', # 'unicycle' or 'holonomic'
     'policy_name': 'sarl-ppo',
-    'distribution': 'gaussian', # 'gaussian', 'dirichlet-bernoulli' or 'dirichlet'
+    'distribution': 'dirichlet', # 'gaussian', 'dirichlet-bernoulli' or 'dirichlet'
     'n_humans': 5, 
     'il_buffer_size': 100_000, # Maximum number of experiences to store in the replay buffer (after exceeding this limit, the oldest experiences are overwritten with new ones)
     'il_training_episodes': 2_000,
@@ -44,7 +45,7 @@ training_hyperparams = {
     'rl_clip_frac': 0.2, # 0.2
     'rl_num_epochs': 10, # 10
     'rl_num_batches': 30, # 30
-    'rl_beta_entropy': 2e-5, 
+    'rl_beta_entropy': 5e-4, # 5e-4
     'lambda_gae': 0.95, # 0.95
     'humans_policy': 'hsfm',
     'scenario': 'hybrid_scenario',
@@ -63,6 +64,7 @@ elif training_hyperparams['reward_function'] == 'socialnav_reward2':
     reward_function = Reward2(
         progress_to_goal_reward = True,
         progress_to_goal_weight = 0.03,
+        v_max = robot_vmax,
         # high_rotation_penalty_reward=True,
         # angular_speed_bound=1.,
         # angular_speed_penalty_weight=0.035,
@@ -86,7 +88,7 @@ env_params = {
 # Initialize environment
 env = SocialNav(**env_params)
 # Initialize robot policy and vnet params
-policy = SARLPPO(env.reward_function, dt=env_params['robot_dt'], kinematics=env_params['kinematics'], distribution=training_hyperparams['distribution'])
+policy = SARLPPO(env.reward_function, v_max=robot_vmax, dt=env_params['robot_dt'], kinematics=env_params['kinematics'], distribution=training_hyperparams['distribution'])
 initial_actor_params = policy.actor.init(training_hyperparams['random_seed'], jnp.zeros((env_params['n_humans'], policy.vnet_input_size)))
 initial_critic_params = policy.critic.init(training_hyperparams['random_seed'], jnp.zeros((env_params['n_humans'], policy.vnet_input_size)))
 # Initialize replay buffer
@@ -125,7 +127,9 @@ il_rollout_params = {
 }
 
 # IMITATION LEARNING ROLLOUT
-il_out = actor_critic_il_rollout(**il_rollout_params)
+# il_out = actor_critic_il_rollout(**il_rollout_params)
+with open(os.path.join(os.path.dirname(__file__),"il_out.pkl"), 'rb') as f:
+    il_out = pickle.load(f)
 
 ## Execute tests to evaluate return after IL
 # Initialize the dictionary to store the metrics
@@ -272,12 +276,12 @@ rl_rollout_params = {
     'debugging_interval': rl_debugging_interval,
 }
 
-# REINFORCEMENT LEARNING ROLLOUT
-rl_out = ppo_rl_rollout(**rl_rollout_params)
+# # REINFORCEMENT LEARNING ROLLOUT
+# rl_out = ppo_rl_rollout(**rl_rollout_params)
 
-# Save RL rollout output
-with open(os.path.join(os.path.dirname(__file__),"rl_out.pkl"), 'wb') as f:
-    pickle.dump(rl_out, f)
+# # Save RL rollout output
+# with open(os.path.join(os.path.dirname(__file__),"rl_out.pkl"), 'wb') as f:
+#     pickle.dump(rl_out, f)
 
 # Load RL rollout output
 with open(os.path.join(os.path.dirname(__file__),"rl_out.pkl"), 'rb') as f:
@@ -464,7 +468,7 @@ for v, visibility in enumerate(test_robot_visibility):
             rl_actor_params, 
             reward_function.time_limit
         )
-        # Store trail metrics
+        # Store trial metrics
         metrics_after_rl = tree_map(lambda x, y: x.at[v,test].set(y), metrics_after_rl, trial_out)
 # Save metrics
 with open(os.path.join(os.path.dirname(__file__),"metrics_after_rl.pkl"), 'wb') as f: