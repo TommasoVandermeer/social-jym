correct bug on grid map computation and bug on SARL*

socialjym/envs/base_env.py
@@ -490,7 +490,7 @@ class BaseEnv(ABC):
         n_y = grid_center_y.shape[1]
         grid_cells = jnp.array(jnp.vstack((grid_center_x.flatten(), grid_center_y.flatten())).T)
         @jit
-        def _obstacle_intersects_cell(x1, y1, x2, y2, xmin, xmax, ymin, ymax):
+        def _edge_intersects_cell(x1, y1, x2, y2, xmin, xmax, ymin, ymax):
             @jit
             def _not_nan_obs(val:tuple):
                 x1, y1, x2, y2, xmin, xmax, ymin, ymax = val
@@ -533,29 +533,27 @@ class BaseEnv(ABC):
                 (x1, y1, x2, y2, xmin, xmax, ymin, ymax)
             )
         @jit
-        def _is_cell_occupied(x1s, y1s, x2s, y2s, xmin, xmax, ymin, ymax):
-            intersects = vmap(_obstacle_intersects_cell, in_axes=(0, 0, 0, 0, None, None, None, None))(x1s, y1s, x2s, y2s, xmin, xmax, ymin, ymax)
-            return jnp.any(intersects)
+        def _obstacle_intersects_cell(obstacle, xmin, xmax, ymin, ymax):
+            return jnp.any(vmap(_edge_intersects_cell, in_axes=(0,0,0,0,None,None,None,None))(obstacle[:,0,0], obstacle[:,0,1], obstacle[:,1,0], obstacle[:,1,1], xmin, xmax, ymin, ymax))
         @jit
-        def _build_occupancy_vector(x1s, y1s, x2s, y2s, xmins, xmaxs, ymins, ymaxs):
+        def _is_cell_occupied(obstacles, xmin, xmax, ymin, ymax):
+            return jnp.any(vmap(_obstacle_intersects_cell, in_axes=(0, None, None, None, None))(obstacles, xmin, xmax, ymin, ymax))
+        @jit
+        def _build_occupancy_vector(obstacles, xmins, xmaxs, ymins, ymaxs):
             """
             Returns a boolean array of shape (n_cells,) indicating whether each cell is occupied (True) or free (False).
 
             parameters:
-            - x1s, y1s, x2s, y2s: Arrays of shape (n_obstacles * n_segments_x_obstacles,) representing the endpoints of the obstacle segments
+            - obstacles: Array of shape (n_obstacles, n_edges, 2, 2) representing the line segments of the obstacles
             - xmins, xmaxs, ymins, ymaxs: Arrays of shape (n_cells,) representing the boundaries of each grid cell
 
             returns:
             - occupancy_vector: Boolean array of shape (n_cells,), where True indicates an occupied cell
             """
-            return vmap(_is_cell_occupied, in_axes=(None, None, None, None, 0, 0, 0, 0))(x1s, y1s, x2s, y2s, xmins, xmaxs, ymins, ymaxs)
+            return vmap(_is_cell_occupied, in_axes=(None, 0, 0, 0, 0))(obstacles, xmins, xmaxs, ymins, ymaxs)
         # Prepare obstacle segments
-        obstacles_reshaped = jnp.reshape(info['static_obstacles'][-1], (self.n_obstacles,-1))  # shape: (n_obstacles * 2, 2)
         occupancy_vector = _build_occupancy_vector(
-            obstacles_reshaped[:,0],
-            obstacles_reshaped[:,1],
-            obstacles_reshaped[:,2],
-            obstacles_reshaped[:,3],
+            info['static_obstacles'][-1],
             grid_cells[:,0] - cell_size/2 - epsilon,
             grid_cells[:,0] + cell_size/2 + epsilon,
             grid_cells[:,1] - cell_size/2 - epsilon,

tests/test_dirsafe_on_ccso.py
@@ -1,9 +1,7 @@
 import jax.numpy as jnp
-import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.colors as mcolors
-from functools import partial
-from jax import random, lax, jit, vmap
+from jax import random, lax, jit, debug
 from jax.tree_util import tree_map
 from jax_tqdm import loop_tqdm
 import os
@@ -154,7 +152,7 @@ def test_on_ccso_with_n_agons(
             state, obs, info, outcome, steps, static_obstacles, all_actions, all_states = while_val
             # Overwrite obstacles if in "circular_crossing_with_static_obstacles" scenario
             aux_info = info.copy()
-            aux_info['static_obstacles'] = static_obstacles # Set obstacles as squares circumscribing static humans
+            aux_info['static_obstacles'] = static_obstacles[env.ccso_n_static_humans:] # Set obstacles as n-agons circumscribing static humans
             aux_obs = obs[env.ccso_n_static_humans:, :] # Remove static humans from observations (so they are not considered as humans by the policy, but only as obstacles)
             # Step the environment
             if policy_type == 0:
@@ -182,6 +180,10 @@ def test_on_ccso_with_n_agons(
         static_obstacles = jnp.array([policy.batch_compute_disk_circumscribing_n_agon(static_humans_positions, static_humans_radii, n_edges=10)])
         nan_obstacles = jnp.full((env.n_humans,) + static_obstacles.shape[1:], jnp.nan)
         static_obstacles = jnp.vstack((nan_obstacles, static_obstacles))
+        if policy.name == 'SARL*':
+            aux_info = info.copy()
+            aux_info['static_obstacles'] = static_obstacles
+            info['grid_cells'], info['occupancy_grid'] = env.build_grid_map_and_occupancy(state, aux_info)
         ## Episode loop
         all_actions = jnp.empty((int(time_limit/env.robot_dt)+1, 2))
         all_states = jnp.empty((int(time_limit/env.robot_dt)+1, env.n_humans+1, 6))
@@ -237,6 +239,21 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), 'dir_safe_benchmar
                 'ccso_n_static_humans': no,
             }
             test_env = SocialNav(**test_env_params)
+            test_env_params_with_grid_map = {
+                'robot_radius': 0.3,
+                'n_humans': no + nh,
+                'n_obstacles': 1, # 1 obstacle needs to be present to enable grid map computation (It is a padding obstacle, NaN)
+                'robot_dt': 0.25,
+                'humans_dt': 0.01,
+                'robot_visible': True,
+                'scenario': 'circular_crossing_with_static_obstacles',
+                'humans_policy': 'hsfm',
+                'reward_function': reward_function,
+                'kinematics': 'unicycle',
+                'ccso_n_static_humans': no,
+                'grid_map_computation': True, # Enable grid map computation for global planning
+            }
+            test_env_with_grid_map = SocialNav(**test_env_params_with_grid_map)
             ## DIR-SAFE tests
             print("\nDIR-SAFE Tests")
             metrics_dir_safe = test_on_ccso_with_n_agons(n_trials, random_seed, test_env, dirsafe, dirsafe_params, time_limit=50.)
@@ -254,21 +271,6 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), 'dir_safe_benchmar
             metrics_hsfm = test_k_trials_hsfm(n_trials, random_seed, test_env, time_limit=50., robot_vmax=robot_vmax, robot_wheels_distance=robot_wheels_distance)
             ## SARL Tests
             print("\nSARL* Tests")
-            test_env_params_with_grid_map = {
-                'robot_radius': 0.3,
-                'n_humans': no + nh,
-                'n_obstacles': 1, # 1 obstacle needs to be present to enable grid map computation (It is a padding obstacle, NaN)
-                'robot_dt': 0.25,
-                'humans_dt': 0.01,
-                'robot_visible': True,
-                'scenario': 'circular_crossing_with_static_obstacles',
-                'humans_policy': 'hsfm',
-                'reward_function': reward_function,
-                'kinematics': 'unicycle',
-                'ccso_n_static_humans': no,
-                'grid_map_computation': True, # Enable grid map computation for global planning
-            }
-            test_env_with_grid_map = SocialNav(**test_env_params_with_grid_map)
             metrics_sarl_star = test_on_ccso_with_n_agons(n_trials, random_seed, test_env_with_grid_map, sarl_star, sarl_params, time_limit=50.)
             ### Store results
             all_metrics = tree_map(lambda x, y: x.at[0,i,j].set(y), all_metrics, metrics_dir_safe)

tests/test_sarl_star.py
@@ -7,6 +7,7 @@ import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning, message="invalid value encountered in cast")
 
 from socialjym.envs.socialnav import SocialNav
+from socialjym.envs.base_env import SCENARIOS
 from socialjym.utils.rewards.socialnav_rewards.reward2 import Reward2
 from socialjym.policies.sarl_star import SARLStar
 from socialjym.utils.aux_functions import animate_trajectory, load_socialjym_policy
@@ -39,6 +40,7 @@ env_params = {
     'reward_function': reward_function,
     'kinematics': kinematics,
     'grid_map_computation': True, # Enable grid map computation for global planning
+    # 'ccso_n_static_humans': 6,
 }
 
 # Initialize and reset environment
@@ -55,26 +57,54 @@ policy = SARLStar(
     wheels_distance=0.7
 )
 policy_params = load_socialjym_policy(os.path.join(os.path.dirname(__file__), 'best_sarl.pkl'))
-# plt.plot(policy.action_space[:,0], policy.action_space[:,1],'.')
-# plt.show()
-
-# Warm up the environment and policy - Dummy step and act to jit compile the functions 
-# (this way, computation time will only reflect execution and not compilation)
-state, _, _, info, _ = env.reset(random.key(0))
-_, obs, _, _, _, _ = env.step(state,info,jnp.zeros((2,)))
-_ = policy.act(random.key(0), obs, info, policy_params, 0.1)
 
 # Simulate some episodes
 for i in range(n_episodes):
     policy_key, reset_key = vmap(random.PRNGKey)(jnp.zeros(2, dtype=int) + random_seed + i) # We don't care if we generate two identical keys, they operate differently
     episode_start_time = time.time()
     state, reset_key, obs, info, outcome = env.reset(reset_key)
+    if info['current_scenario'] == SCENARIOS.index('circular_crossing_with_static_obstacles'):
+        # Compute static obstacles as n-agons circumscribing static humans
+        static_humans_positions = state[0:env.ccso_n_static_humans,0:2]
+        static_humans_radii = info['humans_parameters'][0:env.ccso_n_static_humans,0]
+        static_obstacles = jnp.array([policy.batch_compute_disk_circumscribing_n_agon(
+            static_humans_positions, 
+            static_humans_radii, 
+            n_edges=10
+        )])
+        nan_obstacles = jnp.full((env.n_humans,) + static_obstacles.shape[1:], jnp.nan)
+        static_obstacles = jnp.vstack((nan_obstacles, static_obstacles))
+        aux_info = info.copy()
+        aux_info['static_obstacles'] = static_obstacles # Set obstacles as n-agons circumscribing static humans
+        info['grid_cells'], info['occupancy_grid'] = env.build_grid_map_and_occupancy(state, aux_info)
+        # Find start and end nodes
+        robot_cell_idx, robot_cell_center, robot_cell_occupancy = policy.planner._query_grid_map(info['grid_cells'], info['occupancy_grid'], state[-1,:2])
+        goal_cell_idx, goal_cell_center, goal_cell_occupancy = policy.planner._query_grid_map(info['grid_cells'], info['occupancy_grid'], info['robot_goal'])
+        # Plot occupancy grid, obstacles and shortest path
+        for i, row in enumerate(info['grid_cells']):
+            for j, coord in enumerate(row):
+                cell_size = 0.9
+                facecolor = 'red' if info['occupancy_grid'][i,j] else 'none'
+                facecolor = 'grey' if jnp.array_equal(jnp.array([i,j]), robot_cell_idx) or jnp.array_equal(jnp.array([i,j]), goal_cell_idx) else facecolor
+                rect = plt.Rectangle((coord[0]-cell_size/2, coord[1]-cell_size/2), cell_size, cell_size, facecolor=facecolor, edgecolor='gray', linewidth=0.5, alpha=0.5, zorder=0)
+                plt.gca().add_patch(rect)
+        for o in aux_info['static_obstacles'][-1]: plt.fill(o[:,:,0],o[:,:,1], facecolor='black', edgecolor='black', zorder=3)
+        plt.plot(state[-1,0], state[-1,1], marker='o', color='b', markersize=10, label='Robot start')
+        plt.plot(info['robot_goal'][0], info['robot_goal'][1], marker='*', color='g', markersize=15, label='Robot goal')
+        plt.gca().set_aspect('equal', adjustable='box')
+        plt.show()
     all_states = jnp.array([state])
     while outcome["nothing"]:
-        action, policy_key, _ = policy.act(policy_key, obs, info, policy_params, 0.)
+        if info['current_scenario'] == SCENARIOS.index('circular_crossing_with_static_obstacles'):
+            aux_info = info.copy()
+            aux_info['static_obstacles'] = static_obstacles[env.ccso_n_static_humans:] # Set obstacles as n-agons circumscribing static humans
+            aux_obs = obs[env.ccso_n_static_humans:, :] # Remove static humans from observations (so they are not considered as humans by the policy, but only as obstacles)
+            action, policy_key, _ = policy.act(policy_key, aux_obs, aux_info, policy_params, 0.)
+        else:
+            action, policy_key, _ = policy.act(policy_key, obs, info, policy_params, 0.)
         state, obs, info, reward, outcome, _ = env.step(state,info,action,test=True)
-        print(f"Return in steps [0,{info['step']}):", info["return"], f" - time : {info['time']}")
-        all_states = jnp.vstack((all_states, [state]))
+        # print(f"Return in steps [0,{info['step']}):", info["return"], f" - time : {info['time']}")
+        all_states = jnp.vstack((all_states, jnp.array([state])))
     ## Animate trajectory
     animate_trajectory(
         all_states, 
@@ -85,5 +115,5 @@ for i in range(n_episodes):
         info['current_scenario'],
         robot_dt=env_params['robot_dt'],
         kinematics=kinematics,
-        static_obstacles=info['static_obstacles'][-1],
+        static_obstacles=info['static_obstacles'][-1] if not info['current_scenario'] == SCENARIOS.index('circular_crossing_with_static_obstacles') else static_obstacles[-1],
     )