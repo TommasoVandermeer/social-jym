correcting bugs on new JESSI dataset computation and actorcritic training

scripts/jessi_hcg_to_action.py
@@ -12,8 +12,6 @@ rcParams['pdf.fonttype'] = 42
 rcParams['ps.fonttype'] = 42
 
 from socialjym.policies.jessi import JESSI
-from socialjym.envs.lasernav import LaserNav
-from socialjym.utils.rewards.lasernav_rewards.dummy_reward import DummyReward
 
 save_videos = False  # Whether to save videos of the debug inspections
 ### Parameters
@@ -39,26 +37,10 @@ lidar_num_rays = 100
 scenario = "hybrid_scenario"
 n_humans = 5
 n_obstacles = 3
-env_params = {
-    'robot_radius': 0.3,
-    'n_humans': n_humans,
-    'n_obstacles': n_obstacles,
-    'robot_dt': robot_dt,
-    'robot_radius': robot_radius, 
-    'humans_dt': 0.01,
-    'robot_visible': robot_visible,
-    'scenario': scenario,
-    'reward_function': DummyReward(0.3), # Don't care about reward for now
-    'kinematics': kinematics,
-    'lidar_angular_range':lidar_angular_range,
-    'lidar_max_dist':lidar_max_dist,
-    'lidar_num_rays':lidar_num_rays,
-}
-env = LaserNav(**env_params)
 # Robot jessi
 jessi = JESSI(
     v_max=robot_vmax, 
-    dt=env_params['robot_dt'], 
+    dt=robot_dt, 
     lidar_num_rays=lidar_num_rays, 
     lidar_max_dist=lidar_max_dist,
     lidar_angular_range=lidar_angular_range,
@@ -159,7 +141,7 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), 'controller_networ
         i:int,
         epoch_for_val:tuple,
     ) -> tuple:
-        dataset, actor_critic_params, optimizer_state, losses = epoch_for_val
+        dataset, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = epoch_for_val
         # Shuffle dataset at the beginning of the epoch
         shuffle_key = random.PRNGKey(random_seed + i)
         indexes = jnp.arange(n_data)
@@ -228,7 +210,7 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), 'controller_networ
     avg_losses = jnp.mean(losses, axis=1)
     avg_actor_losses = jnp.mean(actor_losses, axis=1)
     avg_critic_losses = jnp.mean(critic_losses, axis=1)
-    fig, ax = plt.subplots(3, 1, figsize=(18, 6))
+    fig, ax = plt.subplots(1, 3, figsize=(18, 6))
     ax[0].plot(jnp.arange(n_epochs), avg_losses, label="Training Loss")
     ax[1].plot(jnp.arange(n_epochs), avg_actor_losses, label="Actor Loss")
     ax[2].plot(jnp.arange(n_epochs), avg_critic_losses, label="Critic Loss")

scripts/jessi_lidar_to_hcg.py
@@ -17,7 +17,7 @@ from socialjym.policies.jessi import JESSI
 from socialjym.envs.socialnav import SocialNav
 from socialjym.envs.lasernav import LaserNav
 from socialjym.utils.rewards.socialnav_rewards.dummy_reward import DummyReward as SocialNavDummyReward
-from socialjym.utils.rewards.lasernav_rewards.dummy_reward import DummyReward as LaserNavDummyReward
+from socialjym.utils.rewards.lasernav_rewards.reward1 import Reward1 as LaserNavDummyReward
 from jhsfm.hsfm import vectorized_compute_edge_closest_point
 
 ### TODO: Improve training
@@ -184,13 +184,14 @@ def simulate_n_steps(n_steps):
     # Compute returns
     @jit
     def _discounted_cumsum(rewards, dones):
-        def scan_fun(carry, reward, done):
-            new_carry = reward + carry * pow(jessi.gamma, jessi.dt * jessi.v_max) * (1.0 - done)
+        def scan_fun(carry, x):
+            reward, done = x
+            new_carry = reward + carry * jnp.power(jessi.gamma, jessi.dt * jessi.v_max) * (1.0 - done)
             return new_carry, new_carry
-        _, discounted_cumsums = lax.scan(scan_fun, 0.0, rewards[::-1], dones[::-1])
+        _, discounted_cumsums = lax.scan(scan_fun, 0.0, (rewards[::-1], dones[::-1]))
         return discounted_cumsums[::-1]
     dones = jnp.append(data["episode_starts"][1:,:], jnp.zeros((1, n_parallel_envs), dtype=bool), axis=0)
-    data["returns"] = vmap(_discounted_cumsum, in_axes=(1,1))(data["rewards"], dones)
+    data["returns"] = vmap(_discounted_cumsum, in_axes=(1,1))(data["rewards"], dones).T
     data = tree_map(lambda x: x.reshape((-1,) + x.shape[2:]), data)  # Merge parallel envs
     return data
 

socialjym/policies/jessi.py
@@ -654,7 +654,7 @@ class JESSI(BasePolicy):
         )
         # Compute action
         key, subkey = random.split(key)
-        sampled_action, actor_distr, state_value = self.actor_critic.apply(
+        sampled_action, actor_distr, state_value, _ = self.actor_critic.apply(
             actor_critic_params, 
             None, 
             actor_input,
@@ -794,7 +794,7 @@ class JESSI(BasePolicy):
                 returns:jnp.ndarray,
                 ) -> jnp.ndarray:
                 
-                @partial(vmap, in_axes=(None, 0, 0))
+                @partial(vmap, in_axes=(None, 0, 0, 0))
                 def _loss_function(
                     current_actor_params:dict,
                     input:jnp.ndarray,
@@ -814,12 +814,14 @@ class JESSI(BasePolicy):
                                  jnp.exp(-loss_log_vars[1]) * critic_loss + 0.5 * loss_log_vars[1]
                     return total_loss, (actor_loss, critic_loss)
                 
-                return jnp.mean(_loss_function(
+                total_loss, (actor_losses, critic_losses) = _loss_function(
                     current_actor_params,
                     inputs,
                     sample_actions,
                     returns,
-                ))
+                )
+
+                return jnp.mean(total_loss), (jnp.mean(actor_losses), jnp.mean(critic_losses))
 
             inputs = {
                 "actor_input": experiences["actor_inputs"],