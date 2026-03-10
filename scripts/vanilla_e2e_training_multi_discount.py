3a488171fb61a2df827db9307685eecb78ec7c4d
TommasoVandermeer <tommasovdm@gmail.com>
2026-03-10 10:31:24 +0100

    Implement vanilla-e2e multi-gamma training script

--- scripts/vanilla_e2e_training_multi_discount.py
+++ scripts/vanilla_e2e_training_multi_discount.py
@@ -16,15 +16,15 @@ from socialjym.envs.lasernav import LaserNav
 from socialjym.utils.rewards.lasernav_rewards.reward1 import Reward1
 from socialjym.utils.rollouts.vanilla_e2e_rollouts import vanilla_e2e_rl_rollout
 
-unbounded_policy_nn_name = 'vanilla_e2e_il_out.pkl'
-unbounded_network_name = 'vanilla_e2e_rl_out.pkl'
-bounded_policy_nn_name = 'bounded_vanilla_e2e_il_out.pkl'
-bounded_network_name = 'bounded_vanilla_e2e_rl_out.pkl'
+unbounded_network_name = 'vanilla_e2e_multi_gamma_rl_out.pkl'
+bounded_network_name = 'bounded_vanilla_e2e_multi_gamma_rl_out.pkl'
 ### Environment parameters
+seed = 0
 robot_radius = 0.3
 robot_dt = 0.25
 robot_vmax = 1.0
 kinematics = "unicycle"
+n_stack = 5
 lidar_angular_range = 2*jnp.pi
 lidar_max_dist = 10.
 lidar_num_rays = 100
@@ -33,14 +33,6 @@ hybrid_scenario_subset = jnp.array([0,1,2,3,4,6])  # Exclude circular_crossing_w
 n_humans = 5
 n_obstacles = 3
 humans_policy = 'hsfm'
-### IL Hyperparameters
-random_seed = 0
-n_stack = 5  # Number of stacked LiDAR scans as input
-n_steps = 500_000  # Number of labeled examples to train Perception network
-n_parallel_envs = 1000  # Number of parallel environments to simulate to generate the dataset
-policy_learning_rate = 0.005
-policy_batch_size = 200
-policy_n_epochs = 30 # Just a few to not overfit on DIR-SAFE data (if action space becomes too deterministic there will be no exploration in RL fine-tuning)
 ### RL Hyperparameters
 rl_n_parallel_envs = 300 
 rl_training_updates = 500
@@ -69,248 +61,6 @@ training_hyperparams = {
 training_hyperparams['rl_num_batches'] = training_hyperparams['rl_total_batch_size'] // training_hyperparams['rl_mini_batch_size']
 
 
-### PRE-TRAIN UNBOUNDED POLICY NETWORK (IMITATION LEARNING)
-if not os.path.exists(os.path.join(os.path.dirname(__file__), unbounded_policy_nn_name)):
-    # CREATE ACTOR INPUTS DATASET
-    if not os.path.exists(os.path.join(os.path.dirname(__file__), 'controller_training_dataset.pkl')):
-        print("Execute jessi_training.py to generate the initial dataset for Imitation Learning")
-    else:
-        # Load actor inputs
-        with open(os.path.join(os.path.dirname(__file__), 'controller_training_dataset.pkl'), 'rb') as f:
-            # controller_dataset = {
-            #     "observations": 
-            #     "rc_robot_goals":
-            #     "actor_actions":
-            #     "returns":
-            # }
-            controller_dataset = pickle.load(f)
-    # INITIALIZE VanillaE2E
-    policy = VanillaE2E(
-        v_max=robot_vmax, 
-        dt=robot_dt, 
-        lidar_num_rays=lidar_num_rays, 
-        lidar_max_dist=lidar_max_dist,
-        lidar_angular_range=lidar_angular_range,
-        n_stack=n_stack,
-        action_space_bounding=False,
-    )
-    # Initialize actor network
-    actor_critic_params = policy.init_nn(random.PRNGKey(random_seed))
-    # Count network parameters
-    def count_params(actor_critic_params):
-        return sum(jnp.prod(jnp.array(p.shape)) for layer in actor_critic_params.values() for p in layer.values())
-    n_params = count_params(actor_critic_params)
-    print(f"# Controller network parameters: {n_params}")
-    # TRAINING LOOP
-    # Initialize optimizer and its state
-    optimizer = optax.sgd(learning_rate=policy_learning_rate, momentum=0.9)
-    optimizer_state = optimizer.init(actor_critic_params)
-    n_data = controller_dataset["observations"].shape[0]
-    n_train_batches = n_data // policy_batch_size
-    print(f"# Training dataset size: {controller_dataset['observations'].shape[0]} experiences")
-    @loop_tqdm(policy_n_epochs, desc="Training UNBOUNDED Vanilla-E2E policy network")
-    @jit 
-    def _epoch_loop(
-        i:int,
-        epoch_for_val:tuple,
-    ) -> tuple:
-        dataset, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = epoch_for_val
-        # Shuffle dataset at the beginning of the epoch
-        shuffle_key = random.PRNGKey(random_seed + i)
-        indexes = jnp.arange(n_data)
-        shuffled_indexes = random.permutation(shuffle_key, indexes)
-        epoch_data = vmap(lambda idxs, data: tree_map(lambda x: x[idxs], data), in_axes=(0, None))(shuffled_indexes, dataset)
-        # Batch loop
-        @jit
-        def _batch_loop(
-            j:int,
-            batch_for_val:tuple
-        ) -> tuple:
-            epoch_data, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = batch_for_val
-            # Retrieve batch experiences
-            indexes = (jnp.arange(policy_batch_size) + j * policy_batch_size).astype(jnp.int32)
-            batch = vmap(lambda idxs, data: tree_map(lambda x: x[idxs], data), in_axes=(0, None))(indexes, epoch_data)
-            # Compute training batch
-            inputs0, inputs1 = vmap(policy.compute_actor_inputs)(batch["observations"], batch["rc_robot_goals"])
-            train_batch = {
-                "inputs0": inputs0,
-                "inputs1": inputs1,
-                "actor_actions": batch["actor_actions"],
-                "returns": batch["returns"],
-            }
-            # Update parameters
-            actor_critic_params, optimizer_state, loss, actor_loss, critic_loss = policy.update_il(
-                actor_critic_params, 
-                optimizer, 
-                optimizer_state,
-                train_batch,
-            )
-            # Save loss
-            losses = losses.at[i,j].set(loss)
-            actor_losses = actor_losses.at[i,j].set(actor_loss)
-            critic_losses = critic_losses.at[i,j].set(critic_loss)
-            return epoch_data, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses
-        n_batches = n_data // policy_batch_size
-        _, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = lax.fori_loop(
-            0,
-            n_batches,
-            _batch_loop,
-            (epoch_data, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses)
-        )
-        return dataset, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses
-    # Epoch loop
-    _, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = lax.fori_loop(
-        0,
-        policy_n_epochs,
-        _epoch_loop,
-        (controller_dataset, actor_critic_params, optimizer_state, jnp.zeros((policy_n_epochs, int(n_data // policy_batch_size))), jnp.zeros((policy_n_epochs, int(n_data // policy_batch_size))), jnp.zeros((policy_n_epochs, int(n_data // policy_batch_size))))
-    )
-    # Save trained parameters
-    with open(os.path.join(os.path.dirname(__file__), unbounded_policy_nn_name), 'wb') as f:
-        pickle.dump(actor_critic_params, f)
-    # FREE MEMORY
-    del controller_dataset
-    # Plot training loss
-    avg_losses = jnp.mean(losses, axis=1)
-    avg_actor_losses = jnp.mean(actor_losses, axis=1)
-    avg_critic_losses = jnp.mean(critic_losses, axis=1)
-    fig, ax = plt.subplots(1, 3, figsize=(18, 6))
-    ax[0].plot(jnp.arange(policy_n_epochs), avg_losses, c='red')
-    ax[1].plot(jnp.arange(policy_n_epochs), avg_actor_losses, c='green')
-    ax[2].plot(jnp.arange(policy_n_epochs), avg_critic_losses, c='blue')
-    ax[0].set_xlabel("Epoch")
-    ax[0].set_ylabel("Loss (with regularization)")
-    ax[1].set_xlabel("Epoch")
-    ax[1].set_ylabel("Actor Loss (weighted)")
-    ax[2].set_xlabel("Epoch")
-    ax[2].set_ylabel("Critic Loss (weighted)")
-    fig.savefig(os.path.join(os.path.dirname(__file__), unbounded_policy_nn_name.replace('.pkl', '.eps')), format='eps')
-else:
-    # Load trained parameters
-    with open(os.path.join(os.path.dirname(__file__), unbounded_policy_nn_name), 'rb') as f:
-        actor_critic_params = pickle.load(f)
-
-### PRE-TRAIN BOUNDED POLICY NETWORK (IMITATION LEARNING)
-if not os.path.exists(os.path.join(os.path.dirname(__file__), bounded_policy_nn_name)):
-    # CREATE ACTOR INPUTS DATASET
-    if not os.path.exists(os.path.join(os.path.dirname(__file__), 'controller_training_dataset.pkl')):
-        print("Execute jessi_training.py to generate the initial dataset for Imitation Learning")
-    else:
-        # Load actor inputs
-        with open(os.path.join(os.path.dirname(__file__), 'controller_training_dataset.pkl'), 'rb') as f:
-            # controller_dataset = {
-            #     "observations": 
-            #     "rc_robot_goals":
-            #     "actor_actions":
-            #     "returns":
-            # }
-            controller_dataset = pickle.load(f)
-    # INITIALIZE VanillaE2E
-    policy = VanillaE2E(
-        v_max=robot_vmax, 
-        dt=robot_dt, 
-        lidar_num_rays=lidar_num_rays, 
-        lidar_max_dist=lidar_max_dist,
-        lidar_angular_range=lidar_angular_range,
-        n_stack=n_stack,
-        action_space_bounding=True,
-    )
-    # Initialize actor network
-    actor_critic_params = policy.init_nn(random.PRNGKey(random_seed))
-    # Count network parameters
-    def count_params(actor_critic_params):
-        return sum(jnp.prod(jnp.array(p.shape)) for layer in actor_critic_params.values() for p in layer.values())
-    n_params = count_params(actor_critic_params)
-    print(f"# Controller network parameters: {n_params}")
-    # TRAINING LOOP
-    # Initialize optimizer and its state
-    optimizer = optax.sgd(learning_rate=policy_learning_rate, momentum=0.9)
-    optimizer_state = optimizer.init(actor_critic_params)
-    n_data = controller_dataset["observations"].shape[0]
-    n_train_batches = n_data // policy_batch_size
-    print(f"# Training dataset size: {controller_dataset['observations'].shape[0]} experiences")
-    @loop_tqdm(policy_n_epochs, desc="Training BOUNDED Vanilla-E2E policy network")
-    @jit 
-    def _epoch_loop(
-        i:int,
-        epoch_for_val:tuple,
-    ) -> tuple:
-        dataset, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = epoch_for_val
-        # Shuffle dataset at the beginning of the epoch
-        shuffle_key = random.PRNGKey(random_seed + i)
-        indexes = jnp.arange(n_data)
-        shuffled_indexes = random.permutation(shuffle_key, indexes)
-        epoch_data = vmap(lambda idxs, data: tree_map(lambda x: x[idxs], data), in_axes=(0, None))(shuffled_indexes, dataset)
-        # Batch loop
-        @jit
-        def _batch_loop(
-            j:int,
-            batch_for_val:tuple
-        ) -> tuple:
-            epoch_data, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = batch_for_val
-            # Retrieve batch experiences
-            indexes = (jnp.arange(policy_batch_size) + j * policy_batch_size).astype(jnp.int32)
-            batch = vmap(lambda idxs, data: tree_map(lambda x: x[idxs], data), in_axes=(0, None))(indexes, epoch_data)
-            # Compute training batch
-            inputs0, inputs1 = vmap(policy.compute_actor_inputs)(batch["observations"], batch["rc_robot_goals"])
-            train_batch = {
-                "inputs0": inputs0,
-                "inputs1": inputs1,
-                "actor_actions": batch["actor_actions"],
-                "returns": batch["returns"],
-            }
-            # Update parameters
-            actor_critic_params, optimizer_state, loss, actor_loss, critic_loss = policy.update_il(
-                actor_critic_params, 
-                optimizer, 
-                optimizer_state,
-                train_batch,
-            )
-            # Save loss
-            losses = losses.at[i,j].set(loss)
-            actor_losses = actor_losses.at[i,j].set(actor_loss)
-            critic_losses = critic_losses.at[i,j].set(critic_loss)
-            return epoch_data, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses
-        n_batches = n_data // policy_batch_size
-        _, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = lax.fori_loop(
-            0,
-            n_batches,
-            _batch_loop,
-            (epoch_data, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses)
-        )
-        return dataset, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses
-    # Epoch loop
-    _, actor_critic_params, optimizer_state, losses, actor_losses, critic_losses = lax.fori_loop(
-        0,
-        policy_n_epochs,
-        _epoch_loop,
-        (controller_dataset, actor_critic_params, optimizer_state, jnp.zeros((policy_n_epochs, int(n_data // policy_batch_size))), jnp.zeros((policy_n_epochs, int(n_data // policy_batch_size))), jnp.zeros((policy_n_epochs, int(n_data // policy_batch_size))))
-    )
-    # Save trained parameters
-    with open(os.path.join(os.path.dirname(__file__), bounded_policy_nn_name), 'wb') as f:
-        pickle.dump(actor_critic_params, f)
-    # FREE MEMORY
-    del controller_dataset
-    # Plot training loss
-    avg_losses = jnp.mean(losses, axis=1)
-    avg_actor_losses = jnp.mean(actor_losses, axis=1)
-    avg_critic_losses = jnp.mean(critic_losses, axis=1)
-    fig, ax = plt.subplots(1, 3, figsize=(18, 6))
-    ax[0].plot(jnp.arange(policy_n_epochs), avg_losses, c='red')
-    ax[1].plot(jnp.arange(policy_n_epochs), avg_actor_losses, c='green')
-    ax[2].plot(jnp.arange(policy_n_epochs), avg_critic_losses, c='blue')
-    ax[0].set_xlabel("Epoch")
-    ax[0].set_ylabel("Loss (with regularization)")
-    ax[1].set_xlabel("Epoch")
-    ax[1].set_ylabel("Actor Loss (weighted)")
-    ax[2].set_xlabel("Epoch")
-    ax[2].set_ylabel("Critic Loss (weighted)")
-    fig.savefig(os.path.join(os.path.dirname(__file__), bounded_policy_nn_name.replace('.pkl', '.eps')), format='eps')
-else:
-    # Load trained parameters
-    with open(os.path.join(os.path.dirname(__file__), bounded_policy_nn_name), 'rb') as f:
-        actor_critic_params = pickle.load(f)
-
 ### RL UNBOUNDED POLICY NETWORK
 if not os.path.exists(os.path.join(os.path.dirname(__file__), unbounded_network_name)):
     print(f"\nSTARTING UNBOUNDED VANILLA-E2E RL TRAINING\nParallel envs {training_hyperparams['rl_parallel_envs']}\nSteps per env {training_hyperparams['rl_total_batch_size'] // training_hyperparams['rl_parallel_envs']}\nTotal batch size {training_hyperparams['rl_total_batch_size']}\nMini-batch size {training_hyperparams['rl_mini_batch_size']}\nBatches per update {training_hyperparams['rl_num_batches']}\nMicro-batch size {training_hyperparams['rl_micro_batch_size']}\nTraining updates {training_hyperparams['rl_training_updates']}\nEpochs per update {training_hyperparams['rl_num_epochs']}\n")
@@ -319,6 +69,7 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), unbounded_network_
         reward_function = Reward1(
             robot_radius=0.3,
             collision_with_humans_penalty=-.5,
+            gamma=[0.3,0.3,0.5,0.5,0.4,0.9],
         )
     else:
         raise ValueError(f"{training_hyperparams['reward_function']} is not a valid reward function")
@@ -350,10 +101,10 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), unbounded_network_
         lidar_angular_range=lidar_angular_range,
         n_stack=n_stack,
         action_space_bounding=False, 
+        critic_heads=len(reward_function.unique_gammas),
     )
-    # Load pre-trained weights
-    with open(os.path.join(os.path.dirname(__file__), unbounded_policy_nn_name), 'rb') as f:
-        il_network_params = pickle.load(f)
+    # Initialize weights
+    il_network_params = policy.init_nn(random.PRNGKey(seed))
     # Initialize optimizer
     network_optimizer = optax.chain(
         optax.clip_by_global_norm(training_hyperparams['gradient_norm_scale']),
@@ -596,6 +347,7 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), bounded_network_na
         reward_function = Reward1(
             robot_radius=0.3,
             collision_with_humans_penalty=-.5,
+            gamma=[0.3,0.3,0.5,0.5,0.4,0.9],
         )
     else:
         raise ValueError(f"{training_hyperparams['reward_function']} is not a valid reward function")
@@ -627,10 +379,10 @@ if not os.path.exists(os.path.join(os.path.dirname(__file__), bounded_network_na
         lidar_angular_range=lidar_angular_range,
         n_stack=n_stack,
         action_space_bounding=True, 
+        critic_heads=len(reward_function.unique_gammas),
     )
-    # Load pre-trained weights
-    with open(os.path.join(os.path.dirname(__file__), bounded_policy_nn_name), 'rb') as f:
-        il_network_params = pickle.load(f)
+    # Initialize weights
+    il_network_params = policy.init_nn(random.PRNGKey(seed))
     # Initialize optimizer
     network_optimizer = optax.chain(
         optax.clip_by_global_norm(training_hyperparams['gradient_norm_scale']),
